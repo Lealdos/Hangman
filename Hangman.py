028b997e01e0cbfb74e0b49a05cd6154115a56aa
Jose Delgado <j3leonardodelgado@gmail.com>
2022-07-03 15:07:14 -0400

    cleaning words

--- Hangman.py
+++ Hangman.py
@@ -1,36 +1,31 @@
 import random
 import os
-import sys
 import unicodedata
 
 def word_random():
      word = []
      with open("./files/data.txt","r", encoding="utf-8") as f:
-         character = dict.fromkeys (c for c in range(sys.maxunicode) if unicodedata.combining(chr(c)))
-         [word.append(unicodedata.normalize('NFD',words.strip("\n"))) for words in f]
-     palabra = random.choice(word)
-     palabra = "".join(palabra)
-     return palabra.translate(character)
+         [word.append(words.strip("\n")) for words in f]
+     clean_word = random.choice(word)
+     clean_word = ''.join(c for c in unicodedata.normalize('NFD', clean_word) if unicodedata.category(c) != 'Mn')
+     return clean_word
 
 def draw(messege,random_word,lives,charater,hangman):
-
     match messege:
         case 1:
             print(f"YOU WIN! \nThe word is: {random_word}")
         case 2:
             print(f"You already used this character: {charater}\nlives: {lives}\n{hangman}")
         case 3:
-            print(f"You lose, word was:'{random_word}'")
-
-
+            print(f"You lose, word was:{random_word}")
 
 
 def guess(random_word):
     hangman = []
     control_chain=[]
-    control_chain[:0] = random_word 
+    control_chain = [n for n in random_word]
     lives = 5
-    for i in range(len(random_word)):
+    for i in range(len(random_word)): 
         hangman.append("-")
 
     print(hangman)
@@ -39,8 +34,18 @@ def guess(random_word):
             os.system("cls")
             draw(1,random_word,lives,x,hangman)
             break
-        find=False
+        find = False
         x = input("enter a letter: ")
+        while True:
+            if len(x) == 0 or x.isnumeric() or len(x)>1:
+                os.system("cls")
+                print("You can't enter empty chart, a number or longer the one character")
+                print("lives: ",lives)
+                print(hangman)
+                x = input("enter a letter: ")
+                os.system("cls")
+            else: break
+
         x = x.lower()
         os.system("cls")
         if hangman.count(x)!=0:
